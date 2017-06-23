41fa03a5430c33c3683fad8e970aa4578f8b7590
JungYouTaek <jyt9319@gmail.com>
2017-06-23 12:46:39 +0900

    Add Navbar components

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,22 +1,18 @@
-<template>
+<template lang="html">
   <div id="app">
-    <p>{{ message }}</p>
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
+  components: {
+    Navbar
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+
 </style>
---
+++ app/javascript/packs/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+  <nav id="gnb">
+    <h1><img src="assets/lookpin.png" alt="lookpin"></h1>
+    <h2><a href="/">바이슬림</a></h2>
+    <div class="star">
+      <img v-if="counter == true" @click="counter = !counter" src="assets/star.png" alt="star">
+      <img v-else @click="counter = !counter" src="assets/star-clicked.png" alt="star">
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      counter: true
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  #gnb {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #0c1a2c;
+    color: white;
+    width: 100%;
+    height: 75px;
+  }
+  #gnb h1 img {
+    margin-left: 15px;
+    width: 120px;
+    height: 50px;
+  }
+  #gnb h2 {
+    font-size: 40px;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  #gnb a {
+    color: white;
+    text-decoration: none;
+  }
+  .star {
+    margin-right: 30px;
+  }
+  @media (max-width: 504px) {
+    #gnb h1 img {
+      margin-left: 5px;
+      width: 100px;
+      height: 40px;
+    }
+    #gnb h2 {
+      font-size: 30px;
+    }
+  }
+</style>
